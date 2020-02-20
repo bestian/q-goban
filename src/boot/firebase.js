e67dfe5e0fd8c7b84ad6adc7b31cda28082fb73c
Bestian Tang <bestian@gmail.com>
2020-02-20 16:22:52 +0800

    Update firebase.js

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,9 +1,11 @@
 import Firebase from 'firebase/app'
 import 'firebase/firestore' // eslint-disable-line
-import firebaseConfig from '../../firebase.conf'
+// import firebaseConfig from '../../firebase.conf'
 
 export default ({ Vue }) => {
   // Initialize Firebase from settings
-  Firebase.initializeApp(firebaseConfig)
+  if (!Firebase.apps.length) {
+    Firebase.initializeApp({ databaseURL: 'https://goban-hub.firebaseio.com/' })
+  }
   Vue.prototype.$firebase = Firebase
 }
